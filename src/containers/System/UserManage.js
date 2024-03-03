174035900220899a0014f6a71ba4b087c88d3655
Doan Nguyen <gindisme21@gmail.com>
2024-03-04 00:37:46 +0700

    commit

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,18 +3,30 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 class UserManage extends Component {
 
-    state = {
+   constructor(props) {
+      super(props);
+      this.state = {
 
-    }
+      }
+   }
 
     componentDidMount() {
 
     }
 
-
+    /** Life cycle
+     * Run companent :
+     * 1. Run constructor -> init state
+     * 2. Did mount (set state)
+     * 3. Render
+     * 
+     */
     render() {
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className="title text-center">Manage user's with Doan Nguyen </div>
+            </div>
+            
         );
     }
 
