93ffb0f71439c33c56773416a164b6b85c390262
Doan Nguyen <gindisme21@gmail.com>
2024-03-05 23:09:11 +0700

    commit

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
+// import { Fragment } from 'react';
+
 class UserManage extends Component {
 
    constructor(props) {
       super(props);
       this.state = {
-
+        arrUsers: [],
       }
    }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+      let response = await getAllUsers('ALL')
+      if(response && response.errCode === 0){
+        this.setState({
+            arrUsers: response.users
+        })
+      }
     }
 
     /** Life cycle
@@ -22,9 +31,39 @@ class UserManage extends Component {
      * 
      */
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
             <div className="users-container">
                 <div className="title text-center">Manage user's with Doan Nguyen </div>
+                <div className="users-table mt-3 mx-1">
+                <table className="customers">
+                <tr>
+                  <th>Email</th>
+                  <th>Firstname</th>
+                  <th>Lastname</th>
+                  <th>Address</th>
+                  <th>Actions</th>
+                </tr>
+               
+                    {  arrUsers && arrUsers.map((item, index) => {
+                        console.log('doannguyen check map', item, index)
+                          return (
+                            <tr key={index}>
+                            <td>{item.email}</td>
+                            <td>{item.firstName}</td>
+                            <td>{item.lastName}</td>
+                            <td>{item.address}</td>
+                            <td>
+                                <button className="btn-edit"><i className="fas fa-edit"></i></button>
+                                <button className="btn-delete"><i className="fas fa-trash"></i></button>
+                            </td>
+                            </tr>
+                          )
+                      })
+                    } 
+               
+                </table>
+                </div>
             </div>
             
         );
